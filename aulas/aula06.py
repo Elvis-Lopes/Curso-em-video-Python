33345346e95f1ec7741417bd0433800e53479830
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-06 14:46:02 -0300

    exercicios renomeados para ficar de acordo com o curso

---
+++ aulas/aula06.py
@@ -0,0 +1,6 @@
+# nessa aula está revisado os tipos primitivos de dados
+# inserção de valor inteiro em uma variavel e manipulação da variavel
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+print(f'{n1} + {n2} = {s}')
---
+++ exercicios/ex002.py
@@ -0,0 +1,2 @@
+nome = input('Qual é o seu nome?\n').title()
+print(f'è um grande prazer te conhecer, {nome}')
