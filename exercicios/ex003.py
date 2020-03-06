91e12ff43d7233a060cec57acb0f3ef980d56e96
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-06 15:01:45 -0300

    desenvolvimento e resolução dos exercicios 3 e 4

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+# soma de dois números
+n1 = float(input('Insira um valor: '))
+n2 = float(input('Insira um valor: '))
+s = n1 + n2
+print(f'{n1} + {n2} = {s}')
---
+++ exercicios/ex004.py
@@ -0,0 +1,7 @@
+# programa que informa o tipo primitivo mais todas as informações possível sobre a variavel
+var = input('Digite algo: ')
+print(type(var))
+print(var.isalnum())
+print(var.isalpha())
+print(var.isascii())
+print(var.isprintable())
