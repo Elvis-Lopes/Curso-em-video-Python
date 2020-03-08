90a5f093a9ae53624cc74630b488adf7ca416de3
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-08 09:34:24 -0300

    adição dos exercicios 8 ao 10

---
+++ exercicios/ex008.py
@@ -0,0 +1,5 @@
+# conversor de metros para centimetros e milímetros
+metro = float(input('Digite a metragem: '))
+centimetros = metro * 100
+milimetros = metro * 1000
+print(f'{metro} é igual a {centimetros:.0f}cm e {milimetros:.0f} mm')
---
+++ exercicios/ex010.py
@@ -0,0 +1,4 @@
+# conversor de real para dólar ( considerando o valor de 3.27 para o dólar )
+reais = float(input('Digite o valor R$: '))
+valorDolar = reais * 3.27
+print(f'o valor em dolar é US${valorDolar}')
