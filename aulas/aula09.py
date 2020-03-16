f339866e03178d4e69cd1e4e605019dee438c623
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-16 13:49:30 -0300

    aula 09 do Curso em Video - Python

---
+++ aulas/aula09.py
@@ -0,0 +1,28 @@
+frase = "Curso em Video Python"
+
+# fatiando a frase
+print(frase[3:13])
+
+# fatiando e pulando letras
+print(frase[1:15:2])
+
+# contando letra 'o'
+print(frase.count('o'))
+
+# retornando tamanho da frase
+print(len(frase))
+
+# trocando trecho da string
+print(frase.replace('Python', 'Android'))
+
+# deixando a frase inteira em maiusculo
+print(frase.upper())
+
+# deixando a frase inteira em minusculo
+print(frase.lower())
+
+# encontrando a posição de uma palavra na frase
+print(frase.find('Video'))
+
+#dividindo a frase
+print(frase.split())
--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,2 +1,6 @@
 # programa que reproduz mp3
 import pygame
+pygame.init()
+pygame.mixer.music.load('')
+pygame.mixer.music.play()
+pygame.event.wait()
