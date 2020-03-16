c7e4c4c6a4c8dd2b3a875f350e402d00f95e5afc
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-16 14:31:28 -0300

    correção do exercicio 023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,1 +1,11 @@
-# program que leia um número de 0 a 9999 ( como string ) e mostre na tela cada um dos digitos separados
+# program que leia um número de 0 a 9999 mostre na tela cada um dos digitos separados
+num = int(input('Digite um numero: '))
+n = num // 1 % 10
+print(f'analisando o numero {num}')
+print(f'Unidade: {n}')
+n = num // 10 % 10
+print(f'Dezena: {n}')
+n = num // 100 % 10
+print(f'Centena: {n}')
+n = num // 1000 % 10
+print(f'Milhar: {n}')
