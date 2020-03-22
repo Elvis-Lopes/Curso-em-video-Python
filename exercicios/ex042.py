61b635f6b6fd85c6ea91462662e27d11ae6a141f
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-22 09:23:22 -0300

    adição dos exercicio 43

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -2,11 +2,14 @@ r1 = float(input('Insira a reta: '))
 r2 = float(input('Insira a reta: '))
 r3 = float(input('Insira a reta: '))
 
-if r1 < r2 + r3:
-    print('é possível formar um triangulo')
-elif r2 < r1 + r3:
-    print('é possível formar um triangulo')
-elif r3 < r2 + r3:
-    print('é possível formar um triangulo')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('é possível formar um triangulo', end='')
+    if r1 == r2 == r3:
+        print('Equilatero')
+    elif r1 != r2 != r3 != r1:
+        print('Escaleno')
+    else:
+        print('isósceles')
 else:
     print('não é possível formar um triangulo')
+
