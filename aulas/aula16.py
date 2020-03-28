0c4806ba13fed0c8ac215b77252fdd3dd4b447d6
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-28 18:45:31 -0300

    inicio da aulas sobre tuplas

---
+++ aulas/aula16.py
@@ -0,0 +1,8 @@
+# exemplos de Tuplas
+# 1
+lanche = ('Hambúprguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche)
+# mostrando um dos itens da tupla
+print(lanche[1])
+print(lanche[-2])
+print(lanche[1:3])
