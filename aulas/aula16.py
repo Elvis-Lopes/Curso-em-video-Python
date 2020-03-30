202bb2236901547d06334a96e7216367056eb4c2
Elvis Lopes <elvislopes1996@hotmail.com>
2020-03-30 11:55:13 -0300

    finalização da aula 16

--- aulas/aula16.py
+++ aulas/aula16.py
@@ -1,8 +1,18 @@
 # exemplos de Tuplas
 # 1
-lanche = ('Hambúprguer', 'Suco', 'Pizza', 'Pudim')
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
 print(lanche)
 # mostrando um dos itens da tupla
 print(lanche[1])
 print(lanche[-2])
 print(lanche[1:3])
+# usando for para exibir a tupla
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')
+print(sorted(lanche))
+# 2
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
