2f70ebf25bad7b9b31e495ef1b568d4e9384c579
Elvis Lopes <elvislopes1996@hotmail.com>
2020-04-03 19:23:31 -0300

    Correção do exercicio 73

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -5,6 +5,4 @@ times = ('Flamengo', 'Santos', 'Palmeiras', 'Grêmio', 'Athletico-PR', 'São Pau
 print(f'Os cinco primeiros colocados são: {times[0:5]}')
 print(f'Os quatro ultimos são: {times[19:14:-1]}')
 print(f'Em ordem alfabetica: {sorted(times)}')
-for pos, time in enumerate(times):
-    if 'Chapecoense' in times[pos]:
-        print(f'O Chapecoense esta na posição {pos}')
+print(f'O Chapecoense está na posição {times.index("Chapecoense")}')
