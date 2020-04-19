be81effd15aff3e8f41bc3e055267e7760123246
Elvis Lopes <elvislopes1996@hotmail.com>
2020-04-19 10:42:25 -0300

    finalização do exercicio 95

--- exercicios/ex095.py
+++ exercicios/ex095.py
@@ -1,17 +1,46 @@
+time = list()
 jogador = dict()
-listaDeJogadores = list()
-golsFeitos = list()
+partidas = list()
 
 while True:
-    jogador['nome'] = str(input('Nome do jogador: ')).strip().title()
-    jogador['partidas'] = int(input('Quantas partidas jogou? '))
-    for c in range(0, jogador['partidas']):
-        golsFeitos.append(int(input(f'Quantos gols foram feitos na partida {c+1}: ')))
-    jogador['gols'] = golsFeitos[:]
-    listaDeJogadores.append(jogador.copy())
     jogador.clear()
-    golsFeitos.clear()
-
-    opcao = str(input('Deseja continuar ? [S/N]')).strip().upper()[0]
-    if 'N' in opcao:
+    jogador['nome'] = str(input('Nome do jogador: ')).strip().title()
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f'    Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('Erro!\n'
+              'Responda com S ou N')
+    if resp == 'N':
         break
+print('-='*30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*40)
+for k, v in enumerate(time):
+    print(f'{k:>4} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-='*30)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar):'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'Erro! não existe jogador com o código {busca}')
+    else:
+        print(f'-- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-'*40
+          )
