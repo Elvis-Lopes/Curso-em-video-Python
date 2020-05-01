bb074c84943531540a628b95ae7da2ed59171f2b
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-01 09:41:56 -0300

    conclusão do exercicio 105

--- exercicios/ex105.py
+++ exercicios/ex105.py
@@ -2,14 +2,48 @@ def notas(*num, sit=True):
     numeros = list()
     numeros =num[:]
     dicionarioDaSala = dict()
-
+    media = sum(numeros)/len(numeros)
+    desempenho = str()
+    dicionarioDaSala = {'Total: ': len(numeros),
+                        'Maior:': max(numeros),
+                        'Menor:': min(numeros),
+                        'Media: ': media}
     if sit:
+        if media >7:
+            desempenho = 'Excelente'
+            dicionarioDaSala = {'Total: ': len(numeros),
+                                'Maior:': max(numeros),
+                                'Menor:': min(numeros),
+                                'Media: ': media,
+                                'Situação': desempenho}
+        elif media <= 6.9 and media > 5:
+            desempenho = 'Bom'
+            dicionarioDaSala = {'Total: ': len(numeros),
+                                'Maior:': max(numeros),
+                                'Menor:': min(numeros),
+                                'Media: ': media,
+                                'Situaçao: ': desempenho}
+        elif media <= 4.9:
+            desempenho = 'Horrivel'
+            dicionarioDaSala = {'Total: ': len(numeros),
+                                'Maior:': max(numeros),
+                                'Menor:': min(numeros),
+                                'Media: ': media,
+                                'Situaçao: ': desempenho}
+        else:
+            desempenho = ''
+            dicionarioDaSala = {'Total: ': len(numeros),
+                                'Maior:': max(numeros),
+                                'Menor:': min(numeros),
+                                'Media: ': media,
+                                'Situaçao': desempenho}
+    else:
         dicionarioDaSala = {'Total: ': len(numeros),
                             'Maior:': max(numeros),
                             'Menor:': min(numeros),
-                            'Media: ': sum(numeros)/len(numeros)}
+                            'Media: ': media}
     return dicionarioDaSala
 
 
-res = notas(10, 5, 8)
+res = notas(4, 8, 8, sit=False)
 print(res)
