b004ff4449b8dae082dc3d9408e4d67fd2bd66ad
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-03 09:02:58 -0300

    finalização do exercicio 107

--- exercicios/ex107/moeda.py
+++ exercicios/ex107/moeda.py
@@ -1,22 +1,36 @@
-def aumento (valor, aumento):
-    porcentagem = (valor*aumento)/ 100
-    total = valor + porcentagem
-    return moeda(total)
+def aumento (valor, aumento, format=True):
+    total = 0
+    if format:
+        porcentagem = (valor*aumento)/ 100
+        total = valor + porcentagem
+        return moeda(total)
+    else:
+        return total
 
 
-def reduzir (valor, reducao):
-    porcentagem = (valor * reducao) / 100
-    total = valor - porcentagem
-    return moeda(total)
+def reduzir (valor, reducao, format=True):
+    total = 0
+    if format:
+        porcentagem = (valor * reducao) / 100
+        total = valor - porcentagem
+        return moeda(total)
+    else:
+        return total
 
-
-def metade (valor):
+def metade (valor, format=True):
     valor = valor/2
-    return moeda(valor)
+    if format:
+         return moeda(valor)
+    else:
+        return valor
 
-def dobro(valor):
+def dobro(valor, fomart=True):
     valor = valor * 2
-    return moeda(valor)
+    if fomart:
+         return moeda(valor)
+    else:
+        return valor
 
 def moeda(preco=0, moeda='R$' ):
     return f'{moeda}{preco:.2f}'.replace('.',',')
+
