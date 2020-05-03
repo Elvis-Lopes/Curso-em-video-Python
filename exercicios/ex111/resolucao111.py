d6068488e2e5eff6e2ae35f686ed22a0ea4fce44
Elvis Lopes <elvislopes1996@hotmail.com>
2020-05-03 09:29:43 -0300

    inicio do exercicio 111

---
+++ exercicios/ex111/resolucao111.py
@@ -0,0 +1,4 @@
+from exercicios.ex111.utilidadesCeV import moeda
+from exercicios.ex111.utilidadesCeV import dados
+
+moeda.resumo(100, 10, 5)
---
+++ exercicios/ex111/utilidadesCeV/moeda/__init__.py
@@ -0,0 +1,45 @@
+def aumento (valor=0.0, aumento=0.0, format=True):
+    total = 0
+    if format:
+        porcentagem = (valor*aumento)/ 100
+        total = valor + porcentagem
+        return moeda(total)
+    else:
+        return total
+
+
+def reduzir (valor=0.0, reducao=0.0, format=True):
+    total = 0
+    if format:
+        porcentagem = (valor * reducao) / 100
+        total = valor - porcentagem
+        return moeda(total)
+    else:
+        return total
+
+def metade (valor=0.0, format=True):
+    valor = valor/2
+    if format:
+         return moeda(valor)
+    else:
+        return valor
+
+def dobro(valor=0.0, fomart=True):
+    valor = valor * 2
+    if fomart:
+         return moeda(valor)
+    else:
+        return valor
+
+def moeda(preco=0.0, moeda='R$' ):
+    return f'{moeda}{preco:.2f}'.replace('.',',')
+
+def resumo(valor = 0.0, aumen=0.0, reducao=0.0, format=True):
+    print('-='*30)
+    print('Resumo do valor'.center(30))
+    print('-='*30)
+    print(f'Preço Analisado: \t{moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor)}')
+    print(f'Metade do preço: \t{metade(valor)}')
+    print(f'{aumen}% de aumento: \t{aumento(valor, aumen)}')
+    print(f'{reducao}% de redução: \t{reduzir(valor, reducao)}')
